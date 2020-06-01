3fc6336da83c010c840a1aa96d557a2b4fb26708
Benoit Pingris <benoit.pingris@epitech.eu>
2020-06-01 19:45:20 +0200

    improve max json payload to 50mb

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ const morgan = require('morgan')
 
 const app = express()
 app.use(cors())
-app.use(express.json())
-app.use(morgan('common'))
+app.use(express.json({limit: '50mb'}))
 
 const router = express.Router()
 
